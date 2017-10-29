f3c7e16be862f16932e1f2e07c5b213d92f242dc
Marcio da Silva Arantes <marcio.da.silva.arantes@gmail.com>
2017-10-29 13:24:32 -0200

    function for example abstraction and improved documentation

--- MINLP/src/minlp/MINLP.java
+++ MINLP/src/minlp/MINLP.java
@@ -17,16 +17,7 @@ import ilog.cplex.IloCplex;
  */
 public class MINLP extends IloCplex{
     private static final double M = 1e6;    //big M
-    
-    public MINLP() throws IloException {
-    }
 
-    public IloNumVar numVar() throws IloException{
-        return numVar(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-    }
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws IloException {
         // TODO code application logic here
         MINLP cplex = new MINLP();
@@ -34,22 +25,8 @@ public class MINLP extends IloCplex{
         IloNumVar x = cplex.numVar(-4, 3);
         // y in {0,1}
         IloIntVar y = cplex.boolVar();
-        
-        //-------------------[ x*y ]------------------
-        //x*y = v
-        //dedution: if(y=1){ v=x }else{ v=0 }
-        //linear transformation:
-        //  (y-1)*M + x <= v <= x - M(y-1)
-        //  -M*y <= v <= M*y
-        IloNumVar v = cplex.numVar();
-        //v <= x - M(y-1)
-        cplex.addLe(v, cplex.sum(x,cplex.prod(-M,cplex.sum(y,-1))));
-        //v >= x + M(y-1)
-        cplex.addGe(v, cplex.sum(x,cplex.prod(+M,cplex.sum(y,-1))));
-        //v <= +M*y
-        cplex.addLe(v, cplex.prod(+M,y));
-        //v >= -M*y
-        cplex.addGe(v, cplex.prod(-M,y));
+        // v = x*y
+        IloNumVar v = cplex.addProd(x, y);
         
         //------------------[ tests fix some thing ]-----------------
         x.setLB(-2);
@@ -67,5 +44,50 @@ public class MINLP extends IloCplex{
         }
     }
     
+    
+    public MINLP() throws IloException {
+    }
+    /**
+     * create a new continuous variable with default bounds.<br>[lb,ub] &harr [-&#8734,+&#8734]
+     * @return
+     * @throws IloException 
+     */
+    public IloNumVar numVar() throws IloException{
+        return numVar(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+    
+    /**<pre>
+     * define: 
+     *      v = addProd(x,y)  &harr v = x*y
+     * dedution: 
+     *      if(y=1){
+     *          v=x
+     *      }else{ 
+     *          v=0 
+     *      }
+     * linear transformation:
+     *      let v in R
+     *      M*(y-1) + x &le v &le x - M*(y-1)
+     *      -M*y &le v &le M*y
+     * </pre>
+     * @param x a linear expression
+     * @param y a boolean variable {0,1}
+     * @return v as a new continous variable
+     */
+    public IloNumVar addProd(IloNumExpr x, IloIntVar y) throws IloException{
+        if(y.getMax()>1 || y.getMin()<0){
+            throw new IloException("y must be boolean but has interval lb="+y.getMin()+" , ub="+y.getMax()); 
+        }
+        IloNumVar v = numVar();
+        //v <= x - M(y-1)
+        addLe(v, sum(x,prod(-M,sum(y,-1))));
+        //v >= x + M(y-1)
+        addGe(v, sum(x,prod(+M,sum(y,-1))));
+        //v <= +M*y
+        addLe(v, prod(+M,y));
+        //v >= -M*y
+        addGe(v, prod(-M,y));
+        return v;
+    }
     
 }
